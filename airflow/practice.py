eee289f67a5f7fef187b583eb6246761f2e83088
benitmatumona <polviedivine@gmail.com>
2026-07-05 01:39:24 +0200

    added account csv

--- airflow/practice.py
+++ airflow/practice.py
@@ -12,15 +12,22 @@ os.makedirs("data/raw", exist_ok=True)
 account_id = itertools.count(start=200001)
 
 
-with open("data/raw/accounts.csv", "w") as f:
-    for customer_id, customer_date in zip(data["customer_id"], data["join_date"]):
-        account_types = ["Cheque", "Savings", "Credit"]
-        random_number_of_accounts = random.randint(1, 3)
-        random_accounts = random.sample(account_type, random_number_of_accounts)
-        
-        for account_type in random_accounts:
-            start_date = datetime.strptime(customer_date, "%y-%m-%d")
-            end_date = datetime.strptime("2026-06-01", "%y-%m-%d")
-            open_date = fake.date_between(start_date, end_date)
-            f.write(f"{next(account_id)},{customer_id},{account_type},{open_date}\n")
- 
+new_data = {"account_id": [], "customer_id": [], 
+        "account_type": [], "open_date": []}
+
+for row in data.itertuples():
+    account_types = ["Cheque", "Savings", "Credit"]
+    random_number_of_accounts = random.randint(1, 3)
+    random_accounts = random.sample(account_type, random_number_of_accounts)
+    
+    for account_type in random_accounts:
+        start_date = datetime.strptime(data.customer_date, "%Y-%m-%d")
+        end_date = datetime.strptime("2026-06-01", "%Y-%m-%d")
+        open_date = fake.date_between(start_date, end_date)
+        new_data["account_id"].append(next(account_id)),
+        new_data["data.customer_id"].append(data.customer_id)
+        new_data["data.customer_id"].append(account_type),
+        new_data["data.customer_id"].append(open_date)
+
+df = pd.DataFrame(new_data)
+df.to_csv("data/raw/accounts.csv", index=False)
