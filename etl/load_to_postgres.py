a7dc54a15f9a8c6e3e8cbebde43ca82538bc6f81
wtc <bematjhb025@student.wethinkcode.co.za>
2026-07-06 16:58:24 +0200

    restructured folders

--- etl/load_to_postgres.py
+++ etl/load_to_postgres.py
@@ -21,12 +21,12 @@ conn = psycopg2.connect(
 with conn.cursor() as cur:
     for row in customers_df.itertuples():
         cur.execute(
-            "INSERT INTO accounts (customer_id, full_name, province, join_date)"
+            "INSERT INTO customers (customer_id, full_name, province, join_date)"
             f"VALUES ({row.customer_id}, {row.full_name}, {row.province}, {row.join_date});"
             )
         
     for row in accounts_df.itertuples():
         cur.execute(
-            "INSERT INTO accounts (customer_id, full_name, province, join_date)"
-            f"VALUES ({row.customer_id}, {row.full_name}, {row.province}, {row.join_date});"
+            "INSERT INTO accounts (account_id, customer_id, account_type, open_date)"
+            f"VALUES ({row.account_id}, {row.customer_id}, {row.account_type}, {row.open_date});"
             )
