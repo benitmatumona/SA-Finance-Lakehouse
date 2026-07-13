65bacafe3e14341a5d1f212d4579a5095bef9d2d
wtc <bematjhb025@student.wethinkcode.co.za>
2026-07-13 14:59:07 +0200

    restructeed folders

--- data_generator/generate_customers.py
+++ data_generator/generate_customers.py
@@ -1,20 +1,10 @@
 import datetime
 import os
 from faker import Faker
+from src.config import VALID_PROVINCES
 
 
 fake = Faker()
-provinces = [
-    "Gauteng",
-    "Western Cape",
-    "KwaZulu-Natal",
-    "Limpopo",
-    "Mpumalanga",
-    "North West",
-    "Free State",
-    "Northern Cape",
-    "Eastern Cape",
-]
 start_date = datetime.date(2020, 1, 1)
 end_date = datetime.date(2026, 6, 30)
 os.makedirs("data/raw", exist_ok=True)
@@ -23,5 +13,5 @@ with open("data/raw/customers.csv", "w") as f:
     f.write("customer_id,full_name,province,join_date\n")
     for i in range(100001, 101001):
         f.write(
-            f"{i},{fake.name()},{fake.random_element(elements=provinces)},{str(fake.date_between(start_date, end_date))}\n"
+            f"{i},{fake.name()},{fake.random_element(elements=VALID_PROVINCES)},{str(fake.date_between(start_date, end_date))}\n"
         )
